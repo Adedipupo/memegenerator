8b96a8fc8f5adae23341997737ac2757afe8d635
Adedipupo <bayonleolaluwa@gmail.com>
2020-06-30 02:50:17 +0100

    meme

--- src/memeGen.js
+++ src/memeGen.js
@@ -10,7 +10,7 @@ class memeGen extends React.Component {
     render() {
         return (
             <div>
-                Header here!!
+                memeGen here!!
             </div>
         )
     }
