61ab34f5e5f39ba4bc748e41de1d7e86d2a61206
Adedipupo <bayonleolaluwa@gmail.com>
2020-06-30 02:55:39 +0100

    meme

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component{
-    render(){
-        return(
-            <div>
-                Header here!!
-            </div>
-        )
-    }
+const Header = () => {
+return(
+  <div>
+     
+  </div>
+)
 }
 
 export default Header;
